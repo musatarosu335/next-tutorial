d5f60396fbb492bd8d010fdb334c7875bd318dff
Kentaro Sakae <musamusa0607@gmail.com>
2018-04-21 16:50:26 +0900

    Headerの作成

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
+import Header from '../components/Header';
 
 const Index = () => (
   <div>
-    <Link href="/about">
-      <button style={{ fontSize: 20 }}>Go to about page</button>
-    </Link>
+    <Header />
     <p>Hello Next.js</p>
   </div>
 );
